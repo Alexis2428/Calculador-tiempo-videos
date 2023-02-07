c2b29bc5ea4e0a94311b72a54caffdffce8c4aaa
Alexis Landa <114886294+Alexis2428@users.noreply.github.com>
2023-02-07 18:23:47 -0300

    Agrega funciones para agregar/borrar objetos

---
+++ js/main.js
@@ -0,0 +1,59 @@
+function crearVideo() {
+    const indice = $formulario.querySelectorAll('.video').length + 1;
+
+    const $tituloVideo = document.createElement('h5');
+    $tituloVideo.textContent = `Video #${indice}`;
+
+    const $video = document.createElement('div');
+    $video.classList.add('row');
+    $video.classList.add('mb-2');
+    $video.classList.add('video');
+
+    $video.appendChild(crearCuadroTiempo('horas'));
+    $video.appendChild(crearCuadroTiempo('minutos'));
+    $video.appendChild(crearCuadroTiempo('segundos'));
+
+    $formulario.querySelector('#videos').appendChild($tituloVideo);
+    $formulario.querySelector('#videos').appendChild($video);
+}
+
+function crearCuadroTiempo(tiempo) {
+    const $columna = document.createElement('div');
+    $columna.className = 'col';
+
+    const $cuadrotiempo = document.createElement('div');
+    $cuadrotiempo.classList.add('form-floating');
+    $cuadrotiempo.classList.add(tiempo);
+
+    const $cuadroTexto = document.createElement('input');
+    $cuadroTexto.type = 'number';
+    $cuadroTexto.className = 'form-control';
+    $cuadroTexto.id = tiempo;
+    $cuadroTexto.setAttribute('placeholder', tiempo);
+
+    const $texto = document.createElement('label');
+    $texto.setAttribute('for', tiempo);
+    $texto.textContent = `Ingrese la cantidad de ${tiempo} del video`;
+
+    $cuadrotiempo.appendChild($cuadroTexto);
+    $cuadrotiempo.appendChild($texto);
+    $columna.appendChild($cuadrotiempo);
+
+    return $columna;
+}
+
+function borrarUltimoVideo() {
+    const $videos = $formulario.querySelectorAll('.video');
+    const ultimoIndice = $videos.length - 1;
+
+    if (0 <= ultimoIndice) {
+        if (0 === ultimoIndice) {
+            ocultarBotonCalcular();
+        }
+
+        const $titulosVideos = $formulario.querySelectorAll('#videos h5');
+        $videos[ultimoIndice].remove();
+        $titulosVideos[ultimoIndice].remove();
+    }
+}
+
