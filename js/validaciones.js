9170574805e4cfbc1020cba19ca6cd0ef024f34d
Alexis Landa <114886294+Alexis2428@users.noreply.github.com>
2023-02-08 19:03:02 -0300

    Agrega funciones para manejar errores

--- js/validaciones.js
+++ js/validaciones.js
@@ -20,3 +20,52 @@ function validarNumero(numero) {
     return '';
 }
 
+function manejarErrores($numerosTiempo, errores) {
+    let cantidadErrores = 0;
+
+    Object.keys(errores).forEach(function(indice) {
+        const error = errores[indice];
+
+        if (error) {
+            cantidadErrores++;
+            $numerosTiempo[indice].classList.add('error');
+
+            if (!comprobarExisteError(error)) {
+                crearError(error);
+            }
+
+        } else {
+            $numerosTiempo[indice].classList.remove('error');
+        }
+    })
+
+    return cantidadErrores;
+}
+
+function crearError(error) {
+    const $error = document.createElement('li');
+    $error.className = 'list-group-item';
+    $error.textContent = error;
+
+    $formulario.querySelector('#errores').appendChild($error);
+}
+
+function comprobarExisteError(error) {
+    const $listaErrores = $formulario.querySelectorAll('#errores li');
+
+    for (let i = 0; i < $listaErrores.length; i++) {
+        if (error === $listaErrores[i].textContent) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function borrarErroresAnteriores() {
+    const $errores = $formulario.querySelectorAll('#errores li');
+
+    for (let i = 0; i < $errores.length; i++) {
+        $errores[i].remove();
+    }
+}
